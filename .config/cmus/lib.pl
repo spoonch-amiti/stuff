5c5427932e61f39fd1ec503c517fcb0b74042a2d
spoonch <lespoonch@gmail.com>
2024-10-31 00:25:56 +0200

    Add files via upload

---
+++ .config/cmus/lib.pl
@@ -0,0 +1,86 @@
+/zalupa/music/Carpenter Brut/Leather Teeth.mp3
+/zalupa/music/Carpenter Brut/Cheerleader Effect (feat. Kristoffer Rygg).mp3
+/zalupa/music/Carpenter Brut/﻿Sunday Lunch.mp3
+/zalupa/music/Carpenter Brut/Monday Hunt.mp3
+/zalupa/music/Carpenter Brut/Inferno Galore.mp3
+/zalupa/music/Carpenter Brut/Beware The Beast (feat. Mat McNerney).mp3
+/zalupa/music/Carpenter Brut/Hairspray Hurricane.mp3
+/zalupa/music/Carpenter Brut/End Titles.mp3
+/zalupa/music/Carpenter Brut/﻿Opening Title.mp3
+/zalupa/music/Carpenter Brut/﻿Straight Outta Hell.mp3
+/zalupa/music/Carpenter Brut/﻿The Widow Maker (feat. Gunship).mp3
+/zalupa/music/Carpenter Brut/Imaginary Fire (feat. Greg Puciato).mp3
+/zalupa/music/Carpenter Brut/﻿« …Good Night, Goodbye » (feat. Ulver).mp3
+/zalupa/music/Carpenter Brut/﻿Day Stalker.mp3
+/zalupa/music/Carpenter Brut/﻿Night Prowler.mp3
+/zalupa/music/Carpenter Brut/﻿Lipstick Masquerade (feat. Persha).mp3
+/zalupa/music/Carpenter Brut/﻿Color Me Blood.mp3
+/zalupa/music/Carpenter Brut/﻿Stabat Mater (feat. Sylvaine).mp3
+/zalupa/music/Carpenter Brut/﻿Paradisi Gloria.mp3
+/zalupa/music/Carpenter Brut/﻿Leather Terror (feat. Johannes Jonka Andersson).mp3
+/zalupa/music/Carpenter Brut/Escape from Midwich Valley.mp3
+/zalupa/music/Carpenter Brut/Disco Zombi Italia.mp3
+/zalupa/music/Carpenter Brut/L.A. Venice Bitch 80's.mp3
+/zalupa/music/Carpenter Brut/Wake up the President.mp3
+/zalupa/music/Carpenter Brut/347 Midnight Demons.mp3
+/zalupa/music/Carpenter Brut/Le Perv.mp3
+/zalupa/music/Carpenter Brut/Roller Mobster.mp3
+/zalupa/music/Carpenter Brut/Meet Matt Stryker.mp3
+/zalupa/music/Carpenter Brut/Obituary.mp3
+/zalupa/music/Carpenter Brut/Looking for Tracy Tzu.mp3
+/zalupa/music/Carpenter Brut/Sexkiller on the Loose.mp3
+/zalupa/music/Carpenter Brut/Hang'em All.mp3
+/zalupa/music/Carpenter Brut/Division Ruine.mp3
+/zalupa/music/Carpenter Brut/Paradise Warfare.mp3
+/zalupa/music/Carpenter Brut/Run, Sally, Run!.mp3
+/zalupa/music/Carpenter Brut/Turbo Killer.mp3
+/zalupa/music/Carpenter Brut/Anarchy Road.mp3
+/zalupa/music/Carpenter Brut/Invasion A.D..mp3
+/zalupa/music/Tame Impala/Let It Happen.mp3
+/zalupa/music/Tame Impala/Nangs.mp3
+/zalupa/music/Tame Impala/The Moment.mp3
+/zalupa/music/Tame Impala/Yes I'm Changing.mp3
+/zalupa/music/Tame Impala/Eventually.mp3
+/zalupa/music/Tame Impala/Gossip.mp3
+/zalupa/music/Tame Impala/The Less I Know The Better.mp3
+/zalupa/music/Tame Impala/Past Life.mp3
+/zalupa/music/Tame Impala/Disciples.mp3
+/zalupa/music/Tame Impala/'Cause I'm a Man.mp3
+/zalupa/music/Tame Impala/Reality In Motion.mp3
+/zalupa/music/Tame Impala/LoveParanoia.mp3
+/zalupa/music/Tame Impala/﻿New Person, Same Old Mistakes.mp3
+/zalupa/music/Tame Impala/It Is Not Meant to Be.mp3
+/zalupa/music/Tame Impala/Desire Be Desire Go.mp3
+/zalupa/music/Tame Impala/Alter Ego.mp3
+/zalupa/music/Tame Impala/Lucidity.mp3
+/zalupa/music/Tame Impala/Why Won't You Make Up Your Mind.mp3
+/zalupa/music/Tame Impala/Solitude Is Bliss.mp3
+/zalupa/music/Tame Impala/Jeremy's Storm.mp3
+/zalupa/music/Tame Impala/Expectation.mp3
+/zalupa/music/Tame Impala/The Bold Arrow of Time.mp3
+/zalupa/music/Tame Impala/Runway, Houses, City, Clouds.mp3
+/zalupa/music/Tame Impala/I Don't Really Mind.mp3
+/zalupa/music/Tame Impala/Be Above It.mp3
+/zalupa/music/Tame Impala/Endors Toi.mp3
+/zalupa/music/Tame Impala/Apocalypse Dreams.mp3
+/zalupa/music/Tame Impala/Mind Mischief.mp3
+/zalupa/music/Tame Impala/Music to Walk Home By.mp3
+/zalupa/music/Tame Impala/Why Won't They Talk to Me.mp3
+/zalupa/music/Tame Impala/Feels Like We Only Go Backwards.mp3
+/zalupa/music/Tame Impala/Keep on Lying.mp3
+/zalupa/music/Tame Impala/Elephant.mp3
+/zalupa/music/Tame Impala/She Just Won't Believe Me.mp3
+/zalupa/music/Tame Impala/﻿Nothing That Has Happened So Far Has Been Anything We Could Control.mp3
+/zalupa/music/Tame Impala/Sun's Coming Up.mp3
+/zalupa/music/Tame Impala/One More Year.mp3
+/zalupa/music/Tame Impala/Instant Destiny.mp3
+/zalupa/music/Tame Impala/Borderline.mp3
+/zalupa/music/Tame Impala/Posthumous Forgiveness.mp3
+/zalupa/music/Tame Impala/Breathe Deeper.mp3
+/zalupa/music/Tame Impala/Tommorow's Dust.mp3
+/zalupa/music/Tame Impala/﻿On Track.mp3
+/zalupa/music/Tame Impala/Lost In Yesterday.mp3
+/zalupa/music/Tame Impala/Is It True.mp3
+/zalupa/music/Tame Impala/It Might Be Time.mp3
+/zalupa/music/Tame Impala/Glimmer.mp3
+/zalupa/music/Tame Impala/One More Hour.mp3
